fixed gradient

learnable_crf.py
@@ -1,13 +1,13 @@
 import numpy as np
 import pickle
-from scipy.optimize import fmin_l_bfgs_b, fmin_tnc
+from scipy.optimize import fmin_l_bfgs_b
 
 with open('cache/hex.pickle', mode='rb') as h:
     hex_data = pickle.load(h)
 H_e = hex_data['H_e']
 state_edges = hex_data['state_edges']
 state_space = hex_data['state_space']
-state_space = filter(lambda x: x[:20].any(), state_space)  # limit state space to leaf node
+# state_space = filter(lambda x: x[:20].any(), state_space)  # limit state space to leaf node
 S = len(state_space)  # S: state space size
 V = 27  # V: number of nodes
 E = 24  # E: number of edges
@@ -15,17 +15,14 @@ C = 1000.0  # must be float
 
 
 class LearnableCrf:
-    def __init__(self, Phi, Y):
-        assert Phi.min() >= 0 and Phi.max() <= 1
-        assert len(Phi) == len(Y)
-        self.Phi = Phi
-        self.N = len(Phi)
-        self.Y = Y
-        self.theta_old = np.zeros(V + E, dtype=float)
+    def __init__(self, Phi_train, Y_train):
+        assert Phi_train.min() >= 0 and Phi_train.max() <= 1
+        assert len(Phi_train) == len(Y_train)
+        self.Phi = Phi_train
+        self.N = len(Phi_train)
+        self.Y_train = Y_train
         self.opt_theta = fmin_l_bfgs_b(func=self.objective, x0=np.ones(V + E, dtype=float), fprime=self.obj_prime,
-                                       bounds=[(0.5, None)] * (V + E), iprint=0)[0]
-        # self.opt_theta = fmin_tnc(func=self.objective, x0=np.ones(V + E, dtype=float), fprime=self.obj_prime,
-        #                           bounds=[(0.5, None)] * (V + E)).x
+                                       bounds=[(0, None)] * (V + E), epsilon=1e-6, iprint=0)[0]
 
     def update(self, theta):
         def pairwise_step(phi):
@@ -36,36 +33,33 @@ class LearnableCrf:
             return pw
         X = np.tile(self.Phi[:, None, :], (1, S, 1))  # N * V -> N * S * V
         Y_hat = np.tile(state_space, (self.N, 1, 1))  # V * S -> N * S * V
-        unary = (X * Y_hat + (1 - X) * np.logical_not(Y_hat)) / V  # N * S * V
-        pairwise = np.array(map(pairwise_step, self.Phi), dtype=float) / E  # N * S * E
-        W = np.tile(theta[None, None, :V], (self.N, S, 1))  # (V + E) -> N * S * V
-        T = np.tile(theta[None, None, V:], (self.N, S, 1))  # (V + E) -> N * S * E
-        P_tilde = np.exp((W * unary).sum(axis=2) + (T * pairwise).sum(axis=2))  # N * S
-        Z = P_tilde.sum(axis=1)  # R ^ N
-        P_norm = P_tilde / Z[:, None]  # TODO: division error
-        self.theta_old = theta
+        unary = (X * Y_hat + (1 - X) * np.logical_not(Y_hat)) / float(V)  # N * S * V
+        pairwise = np.array(map(pairwise_step, self.Phi), dtype=float) / float(E)  # N * S * E
+        W = np.tile(theta[:V], (self.N, S, 1))  # (V + E) -> N * S * V
+        T = np.tile(theta[V:], (self.N, S, 1))  # (V + E) -> N * S * E
+        P_tilde = np.exp((W * unary).sum(axis=2) +  # N * S * V -> N * S
+                         (T * pairwise).sum(axis=2))  # N * S * E -> N * S
+        P_norm = P_tilde / P_tilde.sum(axis=1)[:, None]  # N * S -> N TODO: division error?
         self.unary = unary
         self.pairwise = pairwise
         self.P_norm = P_norm
 
     def objective(self, theta):
-        if not np.allclose(theta, self.theta_old):
-            self.update(theta)
-        return (-C/self.N) * np.log(self.P_norm[np.arange(self.N), self.Y]).sum() + np.dot(theta-1, theta-1) / 2
+        self.update(theta)
+        return (-C/self.N) * np.log(self.P_norm[np.arange(self.N), self.Y_train]).sum() +np.dot(theta-1, theta-1) / 2.0
 
     def obj_prime(self, theta):
-        if not np.allclose(theta, self.theta_old):
-            self.update(theta)
-        nabla_w_data = self.unary[np.arange(self.N), self.Y, :]
+        self.update(theta)
+        nabla_w_data = self.unary[np.arange(self.N), self.Y_train, :]
         nabla_w_Z = (self.P_norm[:, :, None] * self.unary).sum(axis=1)
         nabla_w = (nabla_w_data - nabla_w_Z).sum(axis=0)
-        nabla_t_data = self.pairwise[np.arange(self.N), self.Y, :]
+        nabla_t_data = self.pairwise[np.arange(self.N), self.Y_train, :]
         nabla_t_Z = (self.P_norm[:, :, None] * self.pairwise).sum(axis=1)
         nabla_t = (nabla_t_data - nabla_t_Z).sum(axis=0)
         return (-C/self.N) * np.concatenate((nabla_w, nabla_t)) + theta - 1
 
-    def predict(self, Phi):
-        self.Phi = Phi
-        self.N = len(Phi)
+    def predict(self, Phi_test):
+        self.Phi = Phi_test
+        self.N = len(Phi_test)
         self.update(self.opt_theta)
         return np.array([state_space[x] for x in self.P_norm.argmax(axis=1)], dtype=bool)

lib.py
@@ -39,8 +39,9 @@ def get_accuracy(Y_predict, Y_truth, lim_states=False):
     Accuracy is also provided in @confusion_matrix. However, this accuracy-only implementation is faster.
     :param Y_predict: N * D array of prediction. Data type may be either numerical or boolean.
     :param Y_truth: N ground truth labels.
+    :param lim_states: for numerical prediction, whether to limit the state space to bottom-level nodes
     """
-    if Y_predict.dtype == bool:  # to limit the states for crf, filter states directly
+    if Y_predict.dtype == bool:  # to limit states for crf, filter the state space directly
         return float(np.count_nonzero(Y_predict[np.arange(len(Y_predict)), Y_truth])) / len(Y_predict)
     if lim_states:
         return float(np.count_nonzero(Y_predict[:, :20].argmax(axis=1) == Y_truth)) / len(Y_predict)